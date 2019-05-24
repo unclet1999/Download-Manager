01e6adbf358ce9cf3b35c52fa9034f4d95c98e68
unclet1999 <real.friend2013@outlook.com>
2019-05-24 15:02:49 +0430

    Tamrin

--- src/com/metal/seyed/MainApp.java
+++ src/com/metal/seyed/MainApp.java
@@ -1,6 +1,8 @@
 package com.metal.seyed;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
--- src/com/metal/seyed/view/AnchorPane1Controller.java
+++ src/com/metal/seyed/view/AnchorPane1Controller.java
@@ -1,7 +1,14 @@
 package com.metal.seyed.view;
 
+import com.metal.seyed.MainApp;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+
+import javax.swing.*;
+
 
 public class AnchorPane1Controller {
 
@@ -50,6 +57,22 @@ public class AnchorPane1Controller {
     @FXML
     private TextField getURL;
 
+    @FXML
+    public void getURLfromTextField(ActionEvent event){
+        try {
+            String t = this.getURL.getText();
+            if (t.equals("") ){
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.initOwner(MainApp.getPrimarystage());
+                alert.setTitle("Error");
+                alert.setHeaderText("Text Field is Empty");
+                alert.setContentText("Please Enter A URL");
+                alert.showAndWait();
+            }
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null , e.getMessage() );
+        }
+    }
 
 
 }
